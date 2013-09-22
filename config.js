0a98f28e74de91a6df9650d90dc4c94b362072e2
Matt Bilker <me@mbilker.us>
2013-09-22 12:27:56 -0400

    Changed servers monitored

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.configuration = [
   { host: 'tekkit.rivalcraft.com', port: 25565 },
-  { host: 'minecraft.rivalcraft.com', port: 25565 },
-  { host: 'rorc.mbilker.us', port: 25565 }
+  { host: 'minecraft.rivalcraft.com', port: 25565 }
+  // { host: 'rorc.mbilker.us', port: 25565 }
 ]
